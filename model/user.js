2e11e9a365107e08b35a06a31ba0fdd2f01d9a56
Shivendra Jadon <shivendra@digicomm.in>
2019-05-29 17:21:48 +0530

    comm

--- model/user.js
+++ model/user.js
@@ -2,12 +2,23 @@ var mysql = require('mysql');
 
 var user = mysql.createPool({
     host : 'localhost',
-    port : '3306',
-    user : 'shivendra.jadon8357@gmail.com',
-    password: 'Jadon@1234',
+    // port : '3306',
+    user : 'root',
+    password: '',
     database : 'neem',
-    // debug : false
+    debug : false
 })
+
+user.on('connection',function(connection){
+    console.log('Connected To DataBase SuccessFully');
+
+user.on('error',function(err){
+    console.log(new Date(), 'MySQL Error' , err.code);
+});
+user.on('close',function(err){
+    console.log(new Date(), 'MySQL close', err);
+});
+});
 
 
 
