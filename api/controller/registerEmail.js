a7d800ace64e51646beaca7b607ab82a406e3f83
Shivendra Jadon <shivendra@digicomm.in>
2019-05-29 12:41:09 +0530

    comm

--- api/controller/registerEmail.js
+++ api/controller/registerEmail.js
@@ -83,8 +83,8 @@ var registerEmail = ((req,res)=>{
                     html: `<body>
                            <h1>Hello ${req.body.email}</h1>
                            <h2>This is your One Time Password : ${otp} </h2>
-                           <a title = "Reset" href="${NEEM}/api/verifyEmail/${emailToken}/${otp}">Click here to verify Your Password</a>
                            `
+                        //    <a title = "Reset" href="${NEEM}/api/verifyEmail/${emailToken}/${otp}">Click here to verify Your Password</a>
                 };
                 transporter.sendMail(mailOptions, function (err,info){
                     if (err) {
@@ -98,7 +98,7 @@ var registerEmail = ((req,res)=>{
                                 return res.json({code : 101 , status : false, message : 'Some Technical Problem', data :''});
                             }else{
                                 
-                                return res.json({code : 100 , status: true,etoken :emailToken,otp : otp, message : 'Succssfully Saved Please Verify Your E-mail Address',data : insert});
+                                return res.json({code : 100 , status: true,otp : otp, message : 'Succssfully Saved Please Verify Your E-mail Address',data : insert});
                             }
                         })  
                         
@@ -122,8 +122,8 @@ var registerEmail = ((req,res)=>{
                         html: `<body>
                                <h1>Hello ${req.body.email}</h1>
                                <h2>This is your One Time Password : ${otp} </h2>
-                               <a title = "Reset" href="${NEEM}/api/verifyEmail/${emailToken}/${otp}">Click here to verify Your Password</a>
                                `
+                            //    <a title = "Reset" href="${NEEM}/api/verifyEmail/${emailToken}/${otp}">Click here to verify Your Password</a>
                     };
                     transporter.sendMail(mailOptions, function (err,info){
                         if (err) {
