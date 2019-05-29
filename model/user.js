b185f7918c432d574b6d492f6bb82841b1e73b28
Shivendra Jadon <shivendra@digicomm.in>
2019-05-29 16:50:20 +0530

    initial

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var user = mysql.createPool({
     host : '127.0.0.1:3306',
-    user : 'shivendra',
+    user : 'shivendra.jadon8357@gmail.com',
     password: 'Jadon@1234',
     database : 'neem',
     // debug : false
