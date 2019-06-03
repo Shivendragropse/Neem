5f7870b74a9d82cbf83ec103534937dfc8e547e4
Shivendra Jadon <shivendra@digicomm.in>
2019-06-03 18:06:38 +0530

    some updates

--- api/controller/fetchUser.js
+++ api/controller/fetchUser.js
@@ -10,6 +10,8 @@ var fetchUser = ((req,res)=>{
             console.log('err',err);
             return res.json({code: 101, status: false , message : 'Some Error'});
         }else {
+      if(!results[0] ||results[0] ==null ||results[0] ==undefined || results[0]=='')return res.send({ status: false, message: "User does not exist"});
+
             console.log('results',results);
             return res.json({
               code : 100,
@@ -34,7 +36,9 @@ var updateProfile = ((req,res)=>{
             if(err){
                 console.log('err1111',err);
                 return res.json({code:101, status : false, message: 'Some Error With Query'});
-            }else {                                
+
+            }else {   
+             if(!results[0] ||results[0] ==null ||results[0] ==undefined || results[0]=='')return res.send({ status: false, message: "User does not exist"});                                             
                 user.query('UPDATE `tb_user` SET firstName = "'+req.body.firstName+'" , lastName = "'+req.body.lastName+'" WHERE email="'+email+'" OR mobile="'+mobile+'"', function(error,data){
                     console.log('dataaaa22222',data);
                     if(error){
